312e410cfce6f32ac33e03564bb4b36a6fb09d28
Lucho <lrdolucho@gmail.com>
2016-04-15 13:27:29 -0400

    First Commit

---
+++ desktop/src/com/epicness/sandbox/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.epicness.sandbox.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.epicness.sandbox.SandBox;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SandBox(), config);
+	}
+}
---
+++ html/src/com/epicness/sandbox/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.epicness.sandbox.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.epicness.sandbox.SandBox;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new SandBox();
+        }
+}
